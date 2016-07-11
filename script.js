7c4c388b325c0f92a906492ef56e7bde597cad39
Tyler Rosenberg <tyryanr@gmail.com>
2016-07-11 18:23:43 -0400

    finally got game working as you click the right colored box

--- script.js
+++ script.js
@@ -8,143 +8,218 @@ var computer = {
   sequence: [],
 }
 
+var clickNumber = 0;
+
 $("#start_button").on("click", function(evt) {
   evt.preventDefault();
-  if (player.inGame == false) {
-    player.round += 1;
-    player.inGame = true;
-    $(".round_number_section").html("Round: " + player.round);
-    generateSequence();
-  } else {
-    $(".message_section").html("<p>You have already started a game. Please click reset if you would like to start over.</p>");
-  }
+  player.round += 1;
+  player.inGame = true;
+  $(".round_number_section").html("<p>Round Number: "+player.round+"</p>");
+
+  generateSequence();
 })
 
 function generateSequence() {
   var newNumber = Math.floor(Math.random() * ((4-0)+1) + 0);
-  if (newNumber == 1) {
+  if (newNumber === 1) {
     computer.sequence.push("green");
-  } else if (newNumber == 2) {
+  } else if (newNumber === 2) {
     computer.sequence.push("red");
-  } else if (newNumber == 3) {
+  } else if (newNumber === 3) {
     computer.sequence.push("yellow");
   } else {
     computer.sequence.push("blue");
   }
-  lightUp();
+  iterator();
 }
 
-function lightUp() {
-  var opac = 500;
+
+function iterator() {
+  var delay = 500;
   for (var i = 0; i < computer.sequence.length; i++) {
-    lighten(computer.sequence[i], opac);
-    opac += 300;
-    dim(opac);
-  }
-  playerTurn();
-}
-
-function lighten(color, opac) {
-  setTimeout(function () {
-    $("#" + color).css("opacity", "1");
-  }, opac);
-}
-
-function dim(opac) {
-  setTimeout(function () {
-    $(".game_button").css("opacity", "0.6");
-  }, opac);
-
-}
-
-function playerTurn() {
-
-  var counter = 0;
-  var playerSequence = [];
-
-  for (var i = 0; i < computer.sequence.length; i++) {
-
-    $(".game_button").on("click", function() {
-      if ($(this).attr("id") == computer.sequence[counter]) {
-        console.log("great");
-        playerSequence.push(computer.sequence[counter]);
-        counter++;
-        if (playerSequence.length == computer.sequence.length) {
-          generateSequence();
-        }
-      } else {
-        console.log("wrong answer");
-      }
-
-
-
-    })
+    lightIn(computer.sequence[i], delay);
+    delay += 500;
+    lightOut(computer.sequence[i], delay);
   }
 }
 
+function lightIn(color, delay) {
+  setTimeout(function () {
+      $("#" + color).css("opacity", "1");
+  }, delay);
+}
+
+function lightOut(color, delay) {
+  setTimeout(function () {
+      $("#" + color).css("opacity", "0.6");
+  }, delay);
+}
 
 
-// The argument computer.sequence[i] above refers to the value of the number at the ith index of the array.
-// It gets passed into the colorShow function below at a set timeout, then all colors opactiy are reset to dim
+$(".game_button").on("click", function() {
+
+  if ($(this).attr("id") == computer.sequence[clickNumber]) {
+  console.log("great job");
+  clickNumber = clickNumber + 1;
+  }
+
+  if (computer.sequence.length == clickNumber) {
+    checker();
+  }
+
+});
+
+function checker() {
+
+    clickNumber = 0;
+    generateSequence();
+    player.round += 1;
+    $(".round_number_section").html("<p>Round Number: "+player.round+"</p>");
+
+}
 
 
+
+
+
+
+
+
+
+// $("#start_button").on("click", function(evt) {
+//   evt.preventDefault();
+//   // if (player.inGame == false) {
+//     player.round += 1;
+//     player.inGame = true;
+//     $(".round_number_section").html("Round: " + player.round);
+//     generateSequence();
+//     lightUp();
+//   // } else {
+//   //   $(".message_section").html("<p>You have already started a game. Please click reset if you would like to start over.</p>");
+//   // }
+// })
 //
-// function colorHide() {
-//   $(".game_button").css("opacity", "0.6");
+// function generateSequence() {
+//   var newNumber = Math.floor(Math.random() * ((4-0)+1) + 0);
+//   if (newNumber == 1) {
+//     computer.sequence.push("green");
+//   } else if (newNumber == 2) {
+//     computer.sequence.push("red");
+//   } else if (newNumber == 3) {
+//     computer.sequence.push("yellow");
+//   } else {
+//     computer.sequence.push("blue");
+//   }
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/***********************
-PSEUDO-CODE
-***********************/
-/*
-
-random button sequence generator function
-generate new number 1-4 and save to variable
-push this new number to the end of the pattern array
-call light up sequence function, passing pattern array through as argument
 //
-
-light up sequence function (pattern)
-for loop go through each iteration of the pattern array and
-$(element).eq(i).opacity-increase
-$(element).eq(i).opacity-decrease to default
+// function lightUp() {
+//   var opac = 500;
+//   for (var i = 0; i < computer.sequence.length; i++) {
+//     lighten(computer.sequence[i], opac);
+//     opac += 300;
+//     dim(opac);
+//   }
+//   // playerTurn();
+// }
 //
-generalMessage = Your turn!
-display general message
-call userTurn function passing pattern through
+// function lighten(color, opac) {
+//   setTimeout(function () {
+//     $("#" + color).css("opacity", "1");
+//   }, opac);
+// }
 //
-
-userTurn function (pattern)
-var userSequence = [];
-for loop length of pattern sequence
-onclick ($anybutton)
-$(this).opacity-increase
-$(this).opacity-decrease to default
-if $(this).eq() equals pattern[i]
-console.log(true)
-streak += 1
-if i = pattern.length - 1
-generalMessage = "awesome job. Next round"
-round += 1
-random button sequence generator function
-else
-generalMessage = "wrong guess nice try. Click start to reset the game"
-break;
+// function dim(opac) {
+//   setTimeout(function () {
+//     $(".game_button").css("opacity", "0.6");
+//   }, opac);
 //
-
-*****************************/
+// }
+//
+// function playerTurn() {
+//
+//   var counter = 0;
+//   var playerSequence = [];
+//
+//   for (var i = 0; i < computer.sequence.length; i++) {
+//
+//     $(".game_button").on("click", function() {
+//       if ($(this).attr("id") == computer.sequence[counter]) {
+//         console.log("great");
+//         playerSequence.push(computer.sequence[counter]);
+//         counter++;
+//         if (playerSequence.length == computer.sequence.length) {
+//           generateSequence();
+//         }
+//       } else {
+//         console.log("wrong answer");
+//       }
+//     })
+//   }
+// }
+//
+//
+//
+// // The argument computer.sequence[i] above refers to the value of the number at the ith index of the array.
+// // It gets passed into the colorShow function below at a set timeout, then all colors opactiy are reset to dim
+//
+//
+// //
+// // function colorHide() {
+// //   $(".game_button").css("opacity", "0.6");
+// // }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// /***********************
+// PSEUDO-CODE
+// ***********************/
+// /*
+//
+// random button sequence generator function
+// generate new number 1-4 and save to variable
+// push this new number to the end of the pattern array
+// call light up sequence function, passing pattern array through as argument
+// //
+//
+// light up sequence function (pattern)
+// for loop go through each iteration of the pattern array and
+// $(element).eq(i).opacity-increase
+// $(element).eq(i).opacity-decrease to default
+// //
+// generalMessage = Your turn!
+// display general message
+// call userTurn function passing pattern through
+// //
+//
+// userTurn function (pattern)
+// var userSequence = [];
+// for loop length of pattern sequence
+// onclick ($anybutton)
+// $(this).opacity-increase
+// $(this).opacity-decrease to default
+// if $(this).eq() equals pattern[i]
+// console.log(true)
+// streak += 1
+// if i = pattern.length - 1
+// generalMessage = "awesome job. Next round"
+// round += 1
+// random button sequence generator function
+// else
+// generalMessage = "wrong guess nice try. Click start to reset the game"
+// break;
+// //
+//
+// *****************************/
