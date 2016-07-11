32335ce3b831b870cd50bb4edce11e37545895db
Tyler Rosenberg <tyryanr@gmail.com>
2016-07-11 15:19:23 -0400

    making changes to setTimeout

--- script.js
+++ script.js
@@ -14,30 +14,81 @@ $("#start_button").on("click", function(evt) {
     player.round += 1;
     player.inGame = true;
     $(".round_number_section").html("Round: " + player.round);
-    setTimeout(generateSequence, 1000);
+    generateSequence();
   } else {
     $(".message_section").html("<p>You have already started a game. Please click reset if you would like to start over.</p>");
   }
 })
 
-
-
 function generateSequence() {
   var newNumber = Math.floor(Math.random() * ((4-0)+1) + 0);
-  computer.sequence.push(newNumber);
+  if (newNumber == 1) {
+    computer.sequence.push("green");
+  } else if (newNumber == 2) {
+    computer.sequence.push("red");
+  } else if (newNumber == 3) {
+    computer.sequence.push("yellow");
+  } else {
+    computer.sequence.push("blue");
+    }
   lightUpSequence();
 }
 
 function lightUpSequence() {
-  for (var i = 0; i < computer.sequence.length; i++) {
-    $(".game_button").eq(i).css("opacity", "1");
-  }
+  // for (var i = 0; i < computer.sequence.length; i++) {
+    // lightSquare(computer.sequence[i]);
+  // }
 
+  var i = 0;
+
+  setTimeout(lightSquare(computer.sequence[i]), 500);
+
+  playerTurn();
 }
 
 
 
 
+
+function lightSquare(color) {
+  $("#" + color).css("opacity", "1");
+  setTimeout(dimSquare, 500);
+}
+
+function dimSquare() {
+  $(".game_button").css("opacity", "0.6");
+}
+// The argument computer.sequence[i] above refers to the value of the number at the ith index of the array.
+// It gets passed into the colorShow function below at a set timeout, then all colors opactiy are reset to dim
+
+
+//
+// function colorHide() {
+//   $(".game_button").css("opacity", "0.6");
+// }
+
+
+function playerTurn() {
+  var playerSequence = [];
+  var counter = 0;
+  $(".message_section").html("<p>Your turn. Good luck!</p>");
+  $(".game_button").on("click", function() {
+     if ($(this).attr("id") == computer.sequence[counter]) {
+        playerSequence.push(computer.sequence[counter]);
+        counter++;
+        console.log("good job");
+     } else {
+       console.log("GAME OVER");
+     }
+
+     if (playerSequence.length == computer.sequence.length) {
+       generateSequence();
+     }
+
+})
+
+
+};
 
 
 
